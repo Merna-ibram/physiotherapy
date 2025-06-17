4e531acc5402524592aa557008bd27019eb9e0f3
Merna Ibram naguib <99305257+Merna-ibram@users.noreply.github.com>
2025-06-17 17:27:44 +0300

    Delete models directory

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import res_partner
-from . import account_move
-from . import my_cases
-from . import res_user
-from . import hr_employee
--- models/res_user.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-
-    # is_specialist = fields.Boolean(string='Is Specialist')
-    patient_ids = fields.One2many('res.partner', 'sales_person', string='My Patients')
